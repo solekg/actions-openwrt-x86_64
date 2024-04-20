6288b847df173d56c02d0c67480488bb32dc9ff1
solekg <118764471+solekg@users.noreply.github.com>
2024-04-20 14:56:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,11 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.201/g' package/base-files/files/bin/config_generate
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
